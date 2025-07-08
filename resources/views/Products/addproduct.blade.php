579e2820c241a56acaced57857be5c2802458ed6
Amin mohamed <aminmohamedaboamasha@gmail.com>
2025-07-08 15:29:21 +0100

    new code

--- resources/views/Products/addproduct.blade.php
+++ resources/views/Products/addproduct.blade.php
@@ -4,12 +4,17 @@
 
 
 
-<div class="col-lg-8 offset-lg-2 text-center">
+<div class="product-section mt-150 mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
 					<div class="section-title">
-						<h3><span class="orange-text">Add</span> Products</h3>
+						<h3><span class="orange-text">Our</span> Products</h3>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
 					</div>
 				</div>
+			</div>
+</div>
 
 
 <div class="contact-from-section mt-150 mb-150">
@@ -22,35 +27,20 @@
 					</div>
 				 	<div id="form_status"></div>
 					<div class="contact-form">
-						<form type="POST" id="fruitkha-contact" onsubmit="return valid_datas( this );">
+						<form method="POST" action="/storeproduct" id="fruitkha-contact" onsubmit="return valid_datas( this );">
+                             @csrf
 							<p>
-								<input type="text" placeholder="Name" name="name" id="name">
-								<input type="email" placeholder="Email" name="email" id="email">
+								<input type="text" style="width: 100%" placeholder="Name" name="name" id="name">
+
 							</p>
-							<p>
-								<input type="tel" placeholder="Phone" name="phone" id="phone">
-								<input type="text" placeholder="Subject" name="subject" id="subject">
+							<p style="display:flex">
+                              <input type="number"  style="width: 50%; margin-right:20px;"placeholder="price" name="price" id="price">
+								<input type="number" style="width: 50%"placeholder="quantity" name="quantity" id="quantity">
+
 							</p>
-							<p><textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea></p>
-							<input type="hidden" name="token" value="FsWga4&amp;@f6aw">
+							<p><textarea name="descraption" id="descraption" cols="30" rows="10" placeholder="descraption"></textarea></p>
 							<p><input type="submit" value="Submit"></p>
 						</form>
-					</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="contact-form-wrap">
-						<div class="contact-form-box">
-							<h4><i class="fas fa-map"></i> Shop Address</h4>
-							<p>34/8, East Hukupara <br> Gifirtok, Sadan. <br> Country Name</p>
-						</div>
-						<div class="contact-form-box">
-							<h4><i class="far fa-clock"></i> Shop Hours</h4>
-							<p>MON - FRIDAY: 8 to 9 PM <br> SAT - SUN: 10 to 8 PM </p>
-						</div>
-						<div class="contact-form-box">
-							<h4><i class="fas fa-address-book"></i> Contact</h4>
-							<p>Phone: +00 111 222 3333 <br> Email: support@fruitkha.com</p>
-						</div>
 					</div>
 				</div>
 			</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Models\category;
 use App\Models\product;
 use App\Http\Controllers\firstController;
 use App\Http\Controllers\addProductController;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,3 +25,18 @@ Route::get('/proudcts/{catid?}',[firstController::class ,'GetcategoryProducts'] 
 Route::get('/category', [firstController::class ,'GetallCategorywithProduct']);
 
 Route::get('/addproduct', [addProductController::class , 'addProduct']);
+
+
+Route::post('/storeproduct', function($request){
+$newProuduct = new product();
+$newProuduct -> name = $request -> name ;
+$newProuduct -> price = $request -> price ;
+$newProuduct -> quantity = $request -> quantity ;
+$newProuduct -> descraption = $request -> descraption ;
+
+$newProuduct ->save();
+
+return redirect('/addprouuct');
+});
+
+
